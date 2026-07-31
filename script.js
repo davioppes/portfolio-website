1bf5f0402a4708df43c1402b4f5a070dc164b2c0
davioppes <do352@exeter.ac.uk>
2026-07-31 23:05:33 +0200

    pretty much done

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ function open_dropdown() {
   document.getElementById("header-items").classList.toggle("show");
 }
 
-
+function open_image(a) {
+  a.parentNode
+    .getElementsByClassName("image-scroll-container")[0]
+    .classList.toggle("show");
+}
